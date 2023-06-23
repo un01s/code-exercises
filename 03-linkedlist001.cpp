1ca636d83478a15e3dd78ddba8518e67bf8ee86a
un01s <96022876+un01s@users.noreply.github.com>
2023-06-23 14:40:34 -0600

    update-linkedlist001

--- 03-linkedlist001.cpp
+++ 03-linkedlist001.cpp
@@ -72,6 +72,32 @@ public:
     }
 };
 
+// use a dummy head
+// so that the operation of removing either the head or the non-head is the same
+
+class Solution2 {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if(cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+
+        delete dummyHead;
+        return head;
+    }
+};
+
 int main() {
     Solution s;
 
