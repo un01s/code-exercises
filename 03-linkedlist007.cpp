35a12a1e62680e92e9a0d58a8014ddb6d1c996e4
un01s <96022876+un01s@users.noreply.github.com>
2023-06-25 15:23:17 -0600

    start-linkedlist007

---
+++ 03-linkedlist007.cpp
@@ -0,0 +1,27 @@
+/**
+ * 142 linked list cycle II
+ * https://leetcode.com/problems/linked-list-cycle-ii/
+ * 
+ * Given the head of a linked list, return the node where the cycle begins. 
+ * If there is no cycle, return null.
+ *
+ */
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
