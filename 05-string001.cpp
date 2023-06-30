82ec840258405c2a56edb8414076350506a22273
un01s <96022876+un01s@users.noreply.github.com>
2023-06-30 15:34:57 -0600

    add-05-string001

---
+++ 05-string001.cpp
@@ -0,0 +1,46 @@
+/** 
+ * 344 reverse string
+ * https://leetcode.com/problems/reverse-string/
+ * 
+ * Write a function that reverses a string. 
+ * The input string is given as an array of characters s.
+ * 
+ * You must do this by modifying the input array in-place with O(1) 
+ * extra memory.
+ * 
+ * 2023-06-30
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    void show(vector<char>& s) {
+        for(int i = 0; i < s.size(); i++) {
+            cout << s[i] << ",";
+        }
+        cout << endl;
+    }
+    void reverseString(vector<char>& s) {
+        for(int i = 0, j = s.size()-1; i < s.size()/2; i++, j--) {
+            swap(s[i], s[j]);
+        }          
+    }
+};
+
+int main() {
+    char s1[] = {'h','e','l','l','o'};
+    vector<char> v(s1, s1+sizeof(s1)/sizeof(char));
+    
+    Solution s;
+    s.show(v);
+    s.reverseString(v);
+    s.show(v);
+
+    return 0;
+}
