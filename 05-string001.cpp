42e822d55d4230f58bc6ed074233e47fcfc103c3
un01s <96022876+un01s@users.noreply.github.com>
2023-06-30 15:36:34 -0600

    update-05-string001

--- 05-string001.cpp
+++ 05-string001.cpp
@@ -28,7 +28,10 @@ public:
     }
     void reverseString(vector<char>& s) {
         for(int i = 0, j = s.size()-1; i < s.size()/2; i++, j--) {
-            swap(s[i], s[j]);
+            //swap(s[i], s[j]);
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
         }          
     }
 };
