79b9d9e3cbb4e1985f6062571970a5b4cc17371e
un01s <96022876+un01s@users.noreply.github.com>
2023-07-02 19:36:37 -0600

    update

---
+++ ex03-76minWindowString.cpp
@@ -0,0 +1,65 @@
+/**
+ * 76 minimum window substring
+ * https://leetcode.com/problems/minimum-window-substring/ 
+ *
+ * Given two strings s and t of lengths m and n respectively, return 
+ * the minimum window substring of s such that every character in t 
+ * (including duplicates) is included in the window. 
+ * If there is no such substring, return the empty string "".
+ * 
+ * The testcases will be generated such that the answer is unique.
+ * 
+ * 2023-07-02 TODO in progress
+ *
+ * order is not important, find out the occurrence, the set, hash-table
+ *  
+ * idea???
+ * use the array, double pointer
+ * 
+ * find all the substrings of s which have every letter of t at least once
+ * then return the shortest substring, the order of letters of t is 
+ * not important
+ * 
+ * set, map(k, v), hash-table
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int sSize = s.size();
+        int tSize = t.size();
+        if (tSize > sSize) {
+            return "";
+        }
+        return "";
+    }
+};
+
+int main() {
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    Solution ans;
+    if ("BANC" == ans.minWindow(s, t)) {
+        cout << "OK\n";
+    } else {
+        cout << "failed\n";
+    }
+
+    string str1 = "a";
+    string t1 = "aa";
+    if ("" == ans.minWindow(str1, t1)) {
+        cout << "OK\n";
+    } else {
+        cout << "failed\n";
+    }
+    return 0;
+}
+
