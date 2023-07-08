f5d9baf3dc18215f2394d141054e0d3ede949dad
un01s <96022876+un01s@users.noreply.github.com>
2023-07-08 16:26:14 -0600

    update

--- exercises/ex10-1.7.cpp
+++ exercises/ex10-1.7.cpp
@@ -1,4 +1,73 @@
 /*
  * implement a queue with two stacks
+ * Leetcode 232 implement queue using stacks
  *
+ * Implement a first in first out (FIFO) queue using only two stacks. 
+ * The implemented queue should support all the functions of a normal queue 
+ * (push, peek, pop, and empty).
+ * 
  */
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> inSt, outSt;
+
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        inSt.push(x);        
+    }
+    
+    int pop() {
+        if(outSt.empty()) {
+            while(!inSt.empty()) {
+                outSt.push(inSt.top());
+                inSt.pop();
+            }
+        }
+        int val = outSt.top();
+        outSt.pop();
+        return val;
+    }
+    
+    int peek() {
+        if (outSt.empty()) {
+            while(!inSt.empty()) {
+                outSt.push(inSt.top());
+                inSt.pop();
+            }
+        }
+        return outSt.top();
+    }
+    
+    bool empty() {
+        return inSt.empty() && outSt.empty();
+    }
+};
+
+int main() {
+    MyQueue* q = new MyQueue();
+
+    q->push(1);
+    q->push(2);
+    q->push(3);
+    if (q->empty()) {
+        cout << "queue is empty" << endl;
+    } else {
+        cout << "queue is NOT empty" << endl;
+    }
+
+    q->pop();
+
+    cout << "peek: " << q->peek() << endl;
+
+    return 0;
+}
--- exercises/ex10-1.8.cpp
+++ exercises/ex10-1.8.cpp
@@ -1,4 +1,115 @@
 /*
  * implement a stack using two queues
+ * 
+ * Leetcode 225
+ *
+ * Implement a last-in-first-out (LIFO) stack using only two queues. 
+ * The implemented stack should support all the functions of a normal stack 
+ * (push, top, pop, and empty).
+ *
  *
  */
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        int qSize = q.size();
+        q.push(x);
+        while(qSize--) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int r = top();
+        q.pop();
+        return r;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+class MyStack2 {
+private:
+    queue<int> q1, q2;
+
+public:
+    MyStack2() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);        
+    }
+    
+    int pop() {
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int r = q1.front();
+        q1.pop();
+        while(!q2.empty()) {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return r;
+    }
+    
+    int top() {
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int r = q1.front();
+        q1.pop();
+        q2.push(r);
+        while(!q2.empty()) {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return r;
+    }
+    
+    bool empty() {
+        if (q1.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    //MyStack* st = new MyStack();
+    MyStack2* st = new MyStack2();
+
+    st->push(1);
+    st->push(2);
+    cout << "stack top : " << st->top() << endl;
+    st->pop();
+
+    if (st->empty()) {
+        cout << "my stack is empty" << endl;
+    } else {
+        cout << "my stack is NOT empty" << endl;
+    }
+
+    return 0;
+}
