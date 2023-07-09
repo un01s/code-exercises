309fc158e6c98b32779721aee0e2018e850e8837
un01s <96022876+un01s@users.noreply.github.com>
2023-07-09 15:23:09 -0600

    update

--- exercises/ex15-146.cpp
+++ exercises/ex15-146.cpp
@@ -2,10 +2,16 @@
  * leetcode 146 LRU cache
  * least recently used cache
  *
+ * the first solution is using the doubly linked list and unordered_map
+ *
+ * the second has the map with a doubly linked list, and another deque!
+ * the unordered_map is better than the map used.
+ *
  */
 
 #include <iostream>
 #include <unordered_map>
+#include <queue> // for deque in the second solution
 
 using namespace std;
 
@@ -22,6 +28,7 @@ public:
     }
 };
 
+// not correct?
 class LRUCache {
 private:
     int cap;
@@ -36,6 +43,7 @@ private:
         n->next = tail;
         tail->prev = n;
         n->prev = beforeTail;
+        // check the capacity of
     }
 
     void removeNode(Node* n) {
@@ -86,16 +94,68 @@ public:
     }
 };
 
+// second solution is not as good as the first!
+class LRUCache2 {
+private:
+    int cacheSize;
+    unordered_map<int, int> m;
+    deque<int> dq;
+
+public:
+    LRUCache2(int capacity) {
+        m.clear();
+        dq.clear();
+        cacheSize = capacity;        
+    }
+    
+    int get(int key) {
+        if (m.find(key) == m.end()) {
+            return -1; // not found
+        } else {
+            deque<int>::iterator it = dq.begin();
+            while(*it != key) {
+                it++;
+            }
+            // update
+            dq.erase(it);
+            dq.push_front(key);
+
+            return m[key];
+        }
+    }
+    
+    void put(int key, int value) {
+        if (m.find(key) == m.end()) {
+            if (cacheSize == dq.size()) {
+                int last = dq.back();
+                dq.pop_back();
+                m.erase(last);
+            }
+        } else {
+            deque<int>::iterator it = dq.begin();
+            while(*it != key) {
+                it++;
+            }
+            dq.erase(it);
+            m.erase(key);
+        }
+        dq.push_front(key);
+        m[key] = value;
+    }
+};
+
 int main() {
  
-    LRUCache* cache = new LRUCache(2);
+    //LRUCache* cache = new LRUCache(2);
+    LRUCache2* cache = new LRUCache2(2);
+
     cache->put(1, 1);
     cache->put(2, 2);
     cout << cache->get(1) << endl; 	// 1
     cache->put(3, 3);
     cout << cache->get(2) << endl;	// -1 not found
     cache->put(4, 4);
-    cout << cache->get(1) << endl; 	// -1
+    cout << cache->get(1) << endl; 	// 1
     cout << cache->get(3) << endl; 	// 3
     cout << cache->get(4) << endl; 	// 4
 
