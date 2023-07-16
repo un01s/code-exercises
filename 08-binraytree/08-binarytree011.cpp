303d56cbd84997f4e1a3b8b6a1c221977202e60a
un01s <96022876+un01s@users.noreply.github.com>
2023-07-16 16:00:44 -0600

    update

---
+++ 08-binraytree/08-binarytree011.cpp
@@ -0,0 +1,62 @@
+/**
+ * Leetcode 513 
+ *
+ * last line, leftest leaf
+ * it may not be a left-leaf, but the leftest leaf at the max depth
+ *
+ * traverse and back-tracking
+ *
+ * 2023-07-16
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth = INT_MIN;
+    int result;
+    void travesal(TreeNode* root, int depth) {
+        if (root->left == nullptr && root->right == nullptr) {
+            if (depth > maxDepth) {
+                maxDepth = depth;
+                result = root->val; // leaf value
+            }
+            return;
+        }
+        if (root->left) {
+            depth++;
+            traversal(root->left, depth);
+            depth--; // back tracking
+        }
+        if (root->right) {
+            depth++;
+            traversal(root->right, depth);
+            depth--; // back tracking
+        }
+        return; 
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        traversal(root, 0); 
+        return result;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
+
