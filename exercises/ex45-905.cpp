32d7a1c6c11f7367b6a36d4c76f42897955e6583
un01s <96022876+un01s@users.noreply.github.com>
2023-07-19 20:47:32 -0600

    update

---
+++ exercises/ex45-905.cpp
@@ -0,0 +1,16 @@
+/* leetcode 905
+ * sort array by parity
+ */
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        for (int i = 0, j = 0; j < nums.size(); ++j) {
+            if (nums[j] % 2 == 0) {
+                swap(nums[i++], nums[j]);
+            }
+        }
+        return nums;
+    }
+};
+
