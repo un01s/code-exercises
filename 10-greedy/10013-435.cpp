58093465cd21b59a9f11cf2361365e2cc0333d0f
un01s <96022876+un01s@users.noreply.github.com>
2023-07-26 20:14:44 -0600

    update;

--- 10-greedy/10013-435.cpp
+++ 10-greedy/10013-435.cpp
@@ -18,7 +18,7 @@ public:
     int eraseOverlapIntervals(vector<vector<int> >& intervals) {
         if(intervals.size() == 0) return 0;
 
-        // sort from little to big, with the left boundary
+        // sort from little to big, with the right boundary
         sort(intervals.begin(), intervals.end(), cmp);
         // 
         int count = 1; // num of non-overlapping intervals
@@ -35,6 +35,50 @@ public:
     }
 };
 
+// the way to use left boudaries is different from using right ones
+// here, count the overlapping.
+// before count non-overlapping
+class Solution2 {
+public:
+    // use left boundary here!
+    static bool cmp(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0]; // from little to big
+    }
+    int eraseOverlapIntervals(vector<vector<int> >& intervals) {
+        if(intervals.size() == 0) return 0;
+
+        // sort from little to big, with the left boundary
+        sort(intervals.begin(), intervals.end(), cmp);
+        int count = 0; // to record the overlapping intervals
+        int end = intervals[0][1]; // right
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] >= end) end = intervals[i][1];
+            else {
+                // overlapping case
+                end = min(end, intervals[i][1]);
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
 int main() {
+    int a[4][2] = {{1,2}, {2, 3}, {3, 4}, {1, 3}};
+    vector<vector<int> > v1(4, vector<int>(2,0));
+    for (int i = 0; i < 4; i++) {
+        for(int j = 0; j < 2; j++) {
+            v1[i][j] = a[i][j];
+        }
+    }
+
+    Solution s;
+
+    if (1 == s.eraseOverlapIntervals(v1)) {
+        cout << "test1 OK" << endl;
+    } else {
+        cout << "test1 failed" << endl;
+    }
+
     return 0;
 }
