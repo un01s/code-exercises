e06f5de0cef888d7c6c745f8c05c8fd1be79331a
un01s <96022876+un01s@users.noreply.github.com>
2023-07-27 11:15:48 -0600

    update

--- 10-greedy/10015-56.cpp
+++ 10-greedy/10015-56.cpp
@@ -12,21 +12,40 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
 class Solution {
 public:
+    // why it does not like lambda?
+    static bool cmp(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    }
     vector<vector<int> > merge(vector<vector<int> >& intervals) {
         vector<vector<int> > res;
+        if (intervals.size() == 0) return res;
+        // sort with lambda, left boundary used
+        //sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        res.push_back(intervals[0]); // first interval
+        for (int i = 1; i < intervals.size(); i++) {
+            if (res.back()[1] > intervals[i][0]) { // right > left
+                // overlapping, merge these two
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]); // non-overlapping
+            }
+        }
         return res;        
     }
 };
 
 int main() {
     int a[4][2] = {{1,3}, {2,6}, {8, 10}, {15,18}};
-    vector<vector<int> > v1(4, vector<int>(2, 0));
-    
+    vector<vector<int> > v1;
     // expect [[1,6], [8,10], [15,18]]
     Solution s;
     vector<vector<int> > a1 = s.merge(v1);
