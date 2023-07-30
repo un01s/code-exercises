769d6156d7d483ca46d1d7580666b4ab4f7f0a38
un01s <96022876+un01s@users.noreply.github.com>
2023-07-29 19:55:34 -0600

    update

---
+++ exercises/ex78-36-valid-sudoku.cpp
@@ -0,0 +1,50 @@
+/*
+ * leetcode valid sudoku
+ *
+ */
+
+class Solution {
+public:
+    bool anyDuplicate(const vector<vector<char>>& board, int start_row, int end_row,
+                      int start_col, int end_col) {
+        bitset<9> is_present;
+        for (int i = start_row; i < end_row; ++i) {
+            for (int j = start_col; j < end_col; ++j) {
+                if (board[i][j] != '.') {
+                    if (is_present[board[i][j] - '1']) {
+                        return true;
+                    }
+                    is_present.flip(board[i][j] - '1');
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // Check row constraints.
+        for (int i = 0; i < 9; ++i) {
+            if (anyDuplicate(board, i, i + 1, 0, 9)) {
+                return false;
+            }
+        }
+
+        // Check column constraints.
+        for (int j = 0; j < board.size(); ++j) {
+            if (anyDuplicate(board, 0, 9, j, j + 1)) {
+                return false;
+            }
+        }
+
+        // Check region constraints.
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (anyDuplicate(board, i, i + 3, j, j + 3)) {
+                    return false;
+                }
+            }
+        }
+        return true;    
+    }
+};
+
