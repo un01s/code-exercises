a6829a1591e3b894c019e8a6f04f0b5f73b0876e
un01s <96022876+un01s@users.noreply.github.com>
2023-07-30 12:35:16 -0600

    update

---
+++ 11-dp/11006-343.cpp
@@ -0,0 +1,26 @@
+/*
+ * leetcode 343
+ * integer break
+ * 
+ * Given an integer n, break it into the sum of k positive integers, 
+ * where k >= 2, and maximize the product of those integers.
+ * 
+ * Return the maximum product you can get.
+ * 
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
