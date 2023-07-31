e70d36edad72dcdf9265da85a00e17e66fd50b15
un01s <96022876+un01s@users.noreply.github.com>
2023-07-31 09:56:39 -0600

    update

--- exercises/ex81-2799.cpp
+++ exercises/ex81-2799.cpp
@@ -3,4 +3,28 @@
  * 2799. Count Complete Subarrays in an Array
  */
 
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+        }
+        int d = m.size();
+        m.clear();
 
+        int c = 0;
+        for(int i = 0; i < n; i++) {
+            m[nums[i]]++;
+            for(int j = i; j< n; j++) {
+                m[nums[j]]++;
+                if (m.size()==d) {
+                    c++;
+                }
+            }
+            m.clear();
+        }        
+        return c;
+    }
+};
