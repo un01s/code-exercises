57e9077f1d966a5bf78319be90359fded839b6e9
un01s <96022876+un01s@users.noreply.github.com>
2023-08-02 09:48:47 -0600

    update

---
+++ 11-dp/11018-139.cpp
@@ -0,0 +1,65 @@
+/*
+ * leetcode 139
+ * dp
+ *
+ * when using backtracking, in at least one case the time limit exceeded
+ * 
+ * s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+ *
+ * wordDict = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+ *
+ */
+
+// back tracking
+// time limit exceeded
+//
+// 
+
+class Solution {
+private:
+    bool backtracking (const string& s, const unordered_set<string>& wordSet, int startIndex) {
+        if (startIndex >= s.size()) {
+            return true;
+        }
+        for (int i = startIndex; i < s.size(); i++) {
+            string word = s.substr(startIndex, i - startIndex + 1);
+            if (wordSet.find(word) != wordSet.end() && backtracking(s, wordSet, i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        return backtracking(s, wordSet, 0);
+    }
+};
+
+// backtracking
+class Solution2 {
+private:
+    bool backtracking (const string& s,
+            const unordered_set<string>& wordSet,
+            vector<bool>& memory,
+            int startIndex) {
+        if (startIndex >= s.size()) {
+            return true;
+        }
+        if (!memory[startIndex]) return memory[startIndex];
+        for (int i = startIndex; i < s.size(); i++) {
+            string word = s.substr(startIndex, i - startIndex + 1);
+            if (wordSet.find(word) != wordSet.end() && backtracking(s, wordSet, memory, i + 1)) {
+                return true;
+            }
+        }
+        memory[startIndex] = false; 
+        return false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        vector<bool> memory(s.size(), 1); 
+        return backtracking(s, wordSet, memory, 0);
+    }
+};
