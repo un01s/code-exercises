e906e53a82038384a9375a7790675c5947136055
un01s <96022876+un01s@users.noreply.github.com>
2023-08-03 11:02:59 -0600

    update

--- 11-dp/11029-674.cpp
+++ 11-dp/11029-674.cpp
@@ -12,6 +12,7 @@
 
 using namespace std;
 
+// DP
 class Solution {
 public:
     int lengthOfiCLIS(vector<int>& nums) {
@@ -24,6 +25,27 @@ public:
                     dp[i] = dp[i-1] + 1;
                 }
                 if (dp[i] > res) res = dp[i];
+        }
+        return res;
+    }
+};
+
+// greedy
+class Solution2 {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int res = 1;
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]) {
+                // continuous & increasing
+                count++;
+            } else {
+                // reset the counter
+                count = 1;
+            }
+            if (count > res) res = count;
         }
         return res;
     }
