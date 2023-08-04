75da5a699fc493ac9f8007e222f53dad8d103d99
un01s <96022876+un01s@users.noreply.github.com>
2023-08-04 15:20:42 -0600

    update

---
+++ 11-dp/11039-516.cpp
@@ -0,0 +1,38 @@
+/*
+ * leetcode 516
+ *
+ * DP
+ * 
+ * dp[i][j] = max length of palindromic within substring in [i, j] of input
+ * 
+ * if s[i] == s[j], dp[i][j] = dp[i+1][j-1]+2;
+ * if s[i] != s[j], dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+ *
+ * one char's palindromic substring is 1
+ *
+ */
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        // initialize
+        vector<vector<int> > dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) dp[i][i] = 1; 
+        // one char's palindrome is itself, so the length is 1.
+        // traverse order is important
+        // one from the end, the other from opposite direction
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
+
+
