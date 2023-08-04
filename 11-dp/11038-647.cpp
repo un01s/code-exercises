f043cbb225d94c817a0461b70f95fb071ebe86e4
un01s <96022876+un01s@users.noreply.github.com>
2023-08-04 15:09:21 -0600

    update

--- 11-dp/11038-647.cpp
+++ 11-dp/11038-647.cpp
@@ -45,3 +45,24 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    // double-pointer
+    int countSubstring(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            res += extend(s, i, i, s.size()); // i as the mid-point
+            res += extend(s, i, i+1, s.size()); // i+1 as the mid-point
+        }
+        return res;
+    }
+    int extend(const string& s, int i, int j, int n) {
+        int res = 0;
+        while (i >= 0 && j < n && s[i]==s[j]) {
+            i--;
+            j++;
+            res++;
+        }
+        return res;
+    }
+};
