e046d59281dbaf843e2396a85c2a976d8b3dc23f
un01s <96022876+un01s@users.noreply.github.com>
2023-08-05 07:03:59 -0600

    update

---
+++ 12-monotonicStack/12002-496.cpp
@@ -0,0 +1,38 @@
+/*
+ * leetcode 496
+ *
+ */
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> res(nums1.size(), -1);
+        // empty case
+        if (nums1.size() == 0) return res;
+
+        // non-empty case
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums1.size(); i++) {
+            m[nums1[i]] = i;
+        }
+        st.push(0); // index 0 
+        // start from 1
+        for (int i = 1; i < nums2.size(); i++) {
+            if (nums2[i] <= nums2[st.top()]) {
+                st.push(); // greater, to stack
+            } else {
+                while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                    if (m.count(nums[st.top()]) > 0) {
+                        // each elemet is unique
+                        int index = m[nums2[st.top()]];
+                        res[index] = nums2[i];
+                    }
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return res;
+    }
+};
