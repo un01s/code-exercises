8a8befde625ddebbe00e535fd7e4412e934cc599
un01s <96022876+un01s@users.noreply.github.com>
2023-08-05 09:26:24 -0600

    update

---
+++ 12-monotonicStack/12004-42.cpp
@@ -0,0 +1,30 @@
+/*
+ * leetcode 42
+ * trapping rain water
+ * 
+ * Given n non-negative integers representing an elevation map where the width 
+ * of each bar is 1, compute how much water it can trap after raining.
+ *
+ * input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ * 
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
+ *
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+    }
+};
+
+int main() {
+    return 0;
+}
