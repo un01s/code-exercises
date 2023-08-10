a4c590149ca118eda9634b4b4bbe505b23a5a9a3
un01s <96022876+un01s@users.noreply.github.com>
2023-08-09 18:09:08 -0600

    update

---
+++ 32-array/32013-81-binarysearch.cpp
@@ -0,0 +1,32 @@
+/*
+ * leetcode 81
+ * search in a rotated sorted array II
+ *
+ *
+ */
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size() - 1;
+        // binary search
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) return true;
+            
+            if (nums[mid] < nums[i]) {
+                if (nums[mid] < target && nums[j] >= target) i = mid + 1;
+                else j = mid - 1;
+            } else if (nums[mid] > nums[i]) {
+                if (nums[mid] > target && nums[i] <= target) j = mid - 1;
+                else i = mid + 1;
+            } else {
+                i++;
+            }
+        }
+        
+        return false;    
+    }
+};
+
