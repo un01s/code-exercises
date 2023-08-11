5ddae58991ba3d83ffabe108eb83b44ad132b3e2
un01s <96022876+un01s@users.noreply.github.com>
2023-08-10 20:17:10 -0600

    update

--- 33-binarytree/33004-116.cpp
+++ 33-binarytree/33004-116.cpp
@@ -8,8 +8,30 @@
  * 
  * input: root = [1, 2, 3, 4, 5, 6, 7]
  * output: [1, null, 2, 3, null, 4, 5, 6, 7, null]
+ *
+ * imagine this not as a binary tree, because we are going to connect
+ * the gaps between two nodes at the same level (this is perfect binary tree)
  * 
+ *     1
+ *    / \
+ *   2   3
+ *  / \ / \
+ * 4  5 6  7
+ * 
+ * imagine 2 and 3 is one node
+ * then imagine 4 and 5, 5 and 6, and 6 and 7 is three nodes below.
+ * 
+ * this is a 3-nary tree not a binary tree.
+ *
+ * ===============================
+ * JUST use the second solution!!!
+ * ===============================
+ *
  */
+
+#include <iostream>
+
+using namespace std;
 
 class Node {
 public:
@@ -28,9 +50,45 @@ public:
 
 class Solution {
 public:
+    void traverse(Node* left, Node* right) {
+       //
+       if (left == nullptr || right == nullptr) {
+           return;
+       }
+       // preorder: mid
+
+       left->next = right;
+       // connect two children with the same parent node
+       traverse(left->left, left->right);
+       traverse(right->left, right->right);
+       // connect two neighboring nodes with different parent node
+       traverse(left->right, right->left);
+    }
     Node* connect(Node* root) {
-        
+        if (root == nullptr) return root;
+
+        traverse(root->left, root->right);
+        return root;    
     }
 };
 
+// time O(n)
+// space O(n)
+class Solution2 {
+public:
+    Node* connect(Node* root) {
+        if (root==NULL) return NULL;
 
+        if (root->left) root->left->next = root->right;
+        if (root->right && root ->next) root->right->next = root->next->left;
+
+        connect(root->left);
+        connect(root->right);
+
+        return root;
+    }
+};
+
+int main() {
+    return 0;
+}
