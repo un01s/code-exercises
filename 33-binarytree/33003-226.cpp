3670390861f45cdc9bacd6c634fa27664ca6e651
un01s <96022876+un01s@users.noreply.github.com>
2023-08-10 19:35:47 -0600

    update

--- 33-binarytree/33003-226.cpp
+++ 33-binarytree/33003-226.cpp
@@ -4,6 +4,19 @@
  *
  */
 
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+ 
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
@@ -37,3 +50,7 @@ public:
         return root;    
     }
 };
+
+int main() {
+    return 0;
+}
