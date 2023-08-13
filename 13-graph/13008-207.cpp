2c2eba8de61355b39cffb551741834a5cfd6bcc9
un01s <96022876+un01s@users.noreply.github.com>
2023-08-13 15:16:47 -0600

    update

--- 13-graph/13008-207.cpp
+++ 13-graph/13008-207.cpp
@@ -20,10 +20,46 @@
 
 class Solution {
 public:
+    vector<bool> visited;
+    vector<bool> onPath;
+    bool hasCycle = false;
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if (numCourses < 2) return true;
         // build a graph
+        vector<vectot<int> > graph = buildGraph(numCourses, prerequisites);
+
+        visited = vector<bool>(numCourses, false);
+        onPath = vector<bool>(numCourses, false);
+
+        for (int i = 0; i < numCourses: i++) {
+            traverse(graph, i);
+        }
         return !hasCycle;                
     }
     // DFS
+    void traverse(vector<vector,int> >& graph, int s) {
+        if (onPath[s]) hasCylcle = true;
+        if (visited[s] || hasCycle) return; 
+        // pre-order
+        visited[s] = true;
+        onPath[s] = true;
+        for (int t : graph[s]) {
+            traverse(graph, t);
+        }
+        // post-order
+        onPath[s] = false;
+    }
+    // build the graph
+    vector<int>* buildGraph(int numCourses, vector<vector<int> >& prerequisites) {
+        auto* graph = new vector<int>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = vector<int>();
+        }
+        for (auto& edge : prerequisites) {
+            int from = edge[1], to = edge[0];
+            graph[from].push_back(to);
+        }
+        return graph;
+    }
 };
