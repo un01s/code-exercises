d63aee238a39d2fea0f3420373397256e96f5b2e
un01s <96022876+un01s@users.noreply.github.com>
2023-08-13 18:26:18 -0600

    update

---
+++ ex02/ex209-215.cpp
@@ -0,0 +1,46 @@
+/*
+ * leetcode 215
+ * K-th largest element in an array
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+// all elements in a set must be unique
+// multiset will sort automatically
+// every time put a number into the multiset
+// if the size of multiset exceeds K, it will remove the smallest
+//
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        multiset<int> s;
+        for(int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+            if(s.size() > k)
+                s.erase(s.begin());
+        }
+        return *s.begin();
+    }
+};
+
+int main() {
+    int a[] = {3, 2, 1, 5, 6, 4};
+    int k1 = 2;
+    vector<int> v1(a, a+sizeof(a)/sizeof(int));
+
+    //
+    Solution s;
+
+    if (s.findKthLargest(v1, k1)==5) {
+        cout << "Test1 OK" << endl;
+    } else {
+        cout << "Test1 failed" << endl;
+    }
+
+    return 0;
+}
