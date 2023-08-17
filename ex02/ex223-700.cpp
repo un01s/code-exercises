a5444143332ca256949cce40305132874b544777
un01s <96022876+un01s@users.noreply.github.com>
2023-08-17 11:01:09 -0600

    update

---
+++ ex02/ex223-700.cpp
@@ -0,0 +1,15 @@
+/*
+ * leetcode 700
+ * search in a binary search tree
+ *
+ */
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == nullptr) return root;
+        if (val == root->val) return root;
+        if (val < root->val) return searchBST(root->left, val);
+        else return searchBST(root->right, val);
+    }
+};
