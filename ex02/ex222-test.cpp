23a5bd98b0209295b581c570831781468fea4d09
un01s <96022876+un01s@users.noreply.github.com>
2023-08-17 09:42:08 -0600

    update

--- ex02/ex222-test.cpp
+++ ex02/ex222-test.cpp
@@ -5,8 +5,40 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+// Find all primes <= n.
+vector<int>Eratosthenes(int n)
+{
+    vector<int>q(n+1,0);
+    vector<int>primes;
+    for (int i=2; i<=sqrt(n); i++)
+    {
+        if (q[i]==1) continue;
+        int j=i*2;
+        while (j<=n)
+        {
+            q[j]=1;
+            j+=i;
+        }
+    }        
+    for (int i=2; i<=n; i++)
+    {
+        if (q[i]==0)
+            primes.push_back(i);                
+    }
+    return primes;
+}
+
+void printV(vector<int>& v) {
+    cout << "V:[ ";
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << "]" << endl;
+}
 
 int main() {
     int a[] = {3, 4, 9, 7, 29, 17, 11, 33};
@@ -15,6 +47,10 @@ int main() {
     // get the max of the array v1
     int m = *max_element(v1.begin(), v1.end());
     cout << "max: " << m << endl;
+
+    // get all the primes less than or equal to n
+    vector<int> p = Eratosthenes(m);
+    printV(p);
 
     return 0;
 }
