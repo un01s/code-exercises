8cfd911596c57d4b18e9a83ab1dbc5ea420fb93c
un01s <96022876+un01s@users.noreply.github.com>
2023-08-19 18:32:01 -0600

    update

---
+++ ex02/ex229-2667.js
@@ -0,0 +1,14 @@
+/*
+ * leetcode 2667. create Hello World function
+ *
+ */
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+       return "Hello World"; 
+    }
+};
+
