9b38688b534d1f4e8c54fb818e12ed76c3c041c9
un01s <96022876+un01s@users.noreply.github.com>
2023-08-26 10:39:05 -0600

    update

--- ex02/ex249-2825.cpp
+++ ex02/ex249-2825.cpp
@@ -1,20 +1,28 @@
 /*
  * leetcode 2825
  * using cyclic increment 'a' -> 'b', 'z'->'a'
+ *
  * for python code, x = ord("h")
  * x is the integer representing the character "h"
+ * the opposite function is chr
+ * c = chr(65)
+ * now, c is 'A'
+ * that means chr take an integer and return its string eqivalent
  * 
  */
 
-class Solution:
-    def canMakeSusequence(self, str1: str, str2: str) -> bool:
-        m, n = len(str1), len(str2)
-        i = j = 0;
-        while i < m and j < n:
-            # check if both match
-            if str1[i] == str2[j] or (ord(str1[i])+1 == ord(str2[j])) or (ord(str1[i])+1-26 == ord(str2[j])):
-                i, j = i+1, j+1
-            else:
-                i += 1
-        return j == n
-
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (str1[i] == str2[j] || str1[i]+1 == str2[j] || str1[i]-25 == str2[j]) {
+                j++;
+            }
+            i++;
+        }
+        return j == m;
+    }
+};
---
+++ ex02/ex250-2825.py
@@ -0,0 +1,25 @@
+/*
+ * leetcode 2825
+ * using cyclic increment 'a' -> 'b', 'z'->'a'
+ *
+ * for python code, x = ord("h")
+ * x is the integer representing the character "h"
+ * the opposite function is chr
+ * c = chr(65)
+ * now, c is 'A'
+ * that means chr take an integer and return its string eqivalent
+ * 
+ */
+
+class Solution:
+    def canMakeSusequence(self, str1: str, str2: str) -> bool:
+        m, n = len(str1), len(str2)
+        i = j = 0;
+        while i < m and j < n:
+            # check if both match
+            if str1[i] == str2[j] or (ord(str1[i])+1 == ord(str2[j])) or (ord(str1[i])+1-26 == ord(str2[j])):
+                i, j = i+1, j+1
+            else:
+                i += 1
+        return j == n
+
