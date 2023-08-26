9c5a9f40c60db7763e1a39dcf8fa94e65b90f1ab
un01s <96022876+un01s@users.noreply.github.com>
2023-08-26 09:49:23 -0600

    update

---
+++ ex02/ex247-2824.cpp
@@ -0,0 +1,40 @@
+/*
+ * leetcode 2824
+ *
+ * https://www.youtube.com/@codeyao9796
+ * 
+ */
+
+// wrong: repeated
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(nums[i]+nums[j] < target) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+        int r = n-1;
+        sort(nums.begin(), nums.end()); // must 
+        for(int i = 0; i < n-1; i++) {
+            while(r > i && (nums[r]+nums[i] >= target)) {
+                r--;
+            }
+            if(r <= i) break;
+            cnt = cnt + r - i;
+        }
+        return cnt;
+    }
+};
+
