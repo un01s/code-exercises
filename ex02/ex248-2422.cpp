8882db1ed432bf7eb722c4c2e758d4a036cd06bc
un01s <96022876+un01s@users.noreply.github.com>
2023-08-26 10:07:04 -0600

    update

--- ex02/ex248-2422.cpp
+++ ex02/ex248-2422.cpp
@@ -1,9 +1,23 @@
 /*
  * leetcode 2422
+ * https://github.com/wisdompeak/LeetCode
+ * https://github.com/akhilkammila/leetcode-screenshotter/
+ * 
+ * merge operations to turn array into a palindrome
+ * nums: positive numbers
+ * ops: choose any two adjacent elements and replace them with their sum
+ * 
+ * return the minimum number of operations needed to turn the array into a
+ * palindrome.
  *
  * double-pointer
  * 
  */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 using LL = long long;
 
@@ -33,3 +47,26 @@ public:
         return res;
     }
 };
+
+int main() {
+    int a[] = {4, 3, 2, 1, 2, 3, 1};
+    vector<int> v1(a, a+sizeof(a)/sizeof(int));
+    int b[] = {1, 2, 3, 4};
+    vector<int> v2(b, b+sizeof(b)/sizeof(int));
+
+    Solution s;
+    
+    if (2 == s.minimumOperations(v1)) {
+        cout << "test1 OK" << endl;
+    } else {
+        cout << "test1 failed" << endl;
+    }
+
+    if (3 == s.minimumOperations(v2)) {
+        cout << "test2 OK" << endl;
+    } else {
+        cout << "test2 failed" << endl;
+    }
+
+    return 0;
+}
