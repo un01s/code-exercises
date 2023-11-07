99c6afeb9c141730533c262a0fb13afe11581c22
un01s <96022876+un01s@users.noreply.github.com>
2023-11-06 19:51:54 -0700

    update

--- ex04/04001.py
+++ ex04/04001.py
@@ -9,3 +9,24 @@ class Solution:
             if c in nums[i+1:]:
                 return [i, nums[i+1:].index(c)+i+1]
 
+# brute force
+#
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, x in enumerate(nums):
+            for j in range(i+1, len(nums)):
+                if x + nums[j] == target:
+                    return [i, j]
+
+
+#
+# hash table
+#
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx = {};
+        for j, x in enumerate(nums):
+            if target-x in idx:
+                return [idx[target-x], j]
+            idx[x] = j
+
