7124d1936a6427ceb9cfef899a5c7d82405ea69c
un01s <96022876+un01s@users.noreply.github.com>
2023-11-06 19:09:52 -0700

    update

--- ex04/04001.cpp
+++ ex04/04001.cpp
@@ -6,18 +6,47 @@
  *
  */
 
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res(2, -1);
         unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); i++) {
             int c = target - nums[i];
             if (m.count(c)) {
-                return {m[c], i};
+                //return {m[c], i};
+                res[0] = m[c];
+                res[1] = i;
+                return res;
             }
             m[nums[i]] = i;
         }
-        return {};
+        return res;
     }
 };
 
+void print(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    return;
+}
+
+int main() {
+    int a[] = {2, 7, 11, 15};
+    int t1 = 9;
+    vector<int> v1(a, a+sizeof(a)/sizeof(int));
+
+    Solution s;
+    vector<int> a1 = s.twoSum(v1, t1);
+    print(a1);
+
+    return 0;
+}
