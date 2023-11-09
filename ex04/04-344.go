769c926342d96ceef0cc802e5b9f4f857f9d80dd
un01s <96022876+un01s@users.noreply.github.com>
2023-11-08 20:36:55 -0700

    update

---
+++ ex04/04-344.go
@@ -0,0 +1,15 @@
+/*
+ * leetcode 344
+ * 
+ */
+
+func reverseString(s []byte) {
+    left := 0
+    right := len(s)-1
+    for left < right {
+        s[left], s[right] = s[right], s[left]
+        left++
+        right--
+    }
+}
+
--- ex04/04-344.py
+++ ex04/04-344.py
@@ -22,4 +22,26 @@ class Solution:
             left += 1
             right -= 1
         
+#
+# use stack
+#
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        stack = []
+        for char in s:
+            stack.append(char)
+        for i in range(len(s)):
+            s[i] = stack.pop()
+
+#
+# use range
+#
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s)
+        for i in range(n//2): # // instead of / in python
+            s[i], s[n-i-1] = s[n-i-1], s[i]
 
