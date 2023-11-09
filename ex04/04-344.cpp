b4c593c0a7cab700a399ec30b873017cbd381ce0
un01s <96022876+un01s@users.noreply.github.com>
2023-11-08 20:31:40 -0700

    update

--- ex04/04-344.cpp
+++ ex04/04-344.cpp
@@ -1,11 +1,28 @@
 /*
  * leetcode 344
+ * 
+ * double-pointer: left and right
+ *
+ * just check the for-loop
+ * the common way is just one invariable like the follows:
+ * for (int i = 0; i < s.size(); i++) { ... }
+ *
  */
 
 class Solution {
 public:
     void reverseString(vector<char>& s) {
         for (int i = 0, j = s.size()-1; i < s.size()/2; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+};
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        for(int i = 0, j = n-1; i < n/2; i++, j--) {
             swap(s[i], s[j]);
         }
     }
---
+++ ex04/04-344.py
@@ -0,0 +1,25 @@
+"""
+leetcode 344
+reverse string
+
+double-pointer
+
+pay attention to some pythonic features
+
+"""
+
+#
+# double pointer: left and right
+#
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
+
