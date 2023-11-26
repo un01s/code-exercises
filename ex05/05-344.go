e1f7218160f1984e79a539badee2a9ea635f089b
un01s <96022876+un01s@users.noreply.github.com>
2023-11-26 13:57:39 -0700

    update

--- ex05/05-344.go
+++ ex05/05-344.go
@@ -19,3 +19,17 @@ func reverseString(s []byte)  {
 	}    
 }
 
+/*
+ * double-pointer
+ */
+
+func reverseString(s []byte)  {
+    left:=0
+    right:=len(s)-1
+    for left<right{
+        s[left],s[right]=s[right],s[left]
+        left++
+        right--
+    }
+}
+
