43dcd59bbd1385181ee00d294cfb99cfd16b627d
un01s <96022876+un01s@users.noreply.github.com>
2023-12-03 14:50:34 -0700

    update

---
+++ ex05/05-704.cpp
@@ -0,0 +1,18 @@
+/* leetcode 704
+ * binary search, array, ordered
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0, j = nums.size()-1;
+        while (i <= j) {
+            int m = i + (j-i)/2;
+            if ( nums[m] < target) i = m+1;
+            else if (nums[m] > target) j = m-1;
+            else return m;
+        }
+        return -1;
+    }
+};
+
