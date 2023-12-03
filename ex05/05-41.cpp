db263ea503628b03b592c12d3b19c36fca6a1d61
un01s <96022876+un01s@users.noreply.github.com>
2023-12-03 13:37:32 -0700

    update

---
+++ ex05/05-41.cpp
@@ -0,0 +1,35 @@
+/*
+ * leetcode 41
+ * first missing positive
+ *
+ */
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int ind = findind(nums,n);
+        return findpos(nums,ind);    
+    }
+    int findind(vector<int> &nums,int n){
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return j;
+    }
+    int findpos(vector<int> &nums,int size){
+        for(int i = 0; i < size; i++){
+            int x = abs(nums[i]);
+            if(x-1 < size && nums[x-1] > 0) nums[x-1] =-nums[x-1];
+        }
+        for(int i = 0; i < size; i++){
+            if(nums[i] > 0) return i+1;
+        }
+        return size+1;
+    }
+};
+
