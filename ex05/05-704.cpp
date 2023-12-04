6cbea2aa51f6dde93b460146ab6edb2e51743f75
un01s <96022876+un01s@users.noreply.github.com>
2023-12-04 09:58:48 -0700

    update

--- ex05/05-704.cpp
+++ ex05/05-704.cpp
@@ -22,3 +22,26 @@ public:
     }
 };
 
+// another binary search
+// this time, [i, j), left closed and right open
+// this means that i is inclusive, while j is not.
+//
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size();
+        while (i < j) {
+            int m = i + (j-i)/2;
+            if (nums[m] > target) {
+                j = m;
+            } else if (nums[m] < target) {
+                i = m+1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+};
+
