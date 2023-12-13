5d9cb310d629dab16827e983472a4f7aadda4b11
un01s <96022876+un01s@users.noreply.github.com>
2023-12-13 10:35:34 -0700

    update

---
+++ ex05/05-707.cpp
@@ -0,0 +1,96 @@
+/*
+ * leetcode 707
+ * linked list design
+ *
+ */
+
+class MyLinkedList {
+    struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
+
+    MyLinkedList() {
+        _dummyHead = new LinkedNode(0); 
+        _size = 0;
+    }
+
+    int get(int index) {
+        if (index > (_size - 1) || index < 0) {
+            return -1;
+        }
+        LinkedNode* cur = _dummyHead->next;
+        while(index--){ // --index causes endless loop
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+
+    void addAtHead(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
+    }
+
+    void addAtTail(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(cur->next != nullptr){
+            cur = cur->next;
+        }
+        cur->next = newNode;
+        _size++;
+    }
+
+    // insert a node right before the index
+    // if index = 0, the new node will be the head of the list
+    // if index is the length of the list, the new node will be the last one
+    void addAtIndex(int index, int val) {
+
+        if(index > _size) return;
+        if(index < 0) index = 0;        
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
+    }
+
+    // index starts from 0, remove the node at the index
+    void deleteAtIndex(int index) {
+        if (index >= _size || index < 0) {
+            return;
+        }
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur ->next;
+        }
+        LinkedNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        // release the memory
+
+        // have to do this to prevent tmp unpredictable
+        tmp=nullptr;
+        _size--;
+    }
+
+    void printLinkedList() {
+        LinkedNode* cur = _dummyHead;
+        while (cur->next != nullptr) {
+            cout << cur->next->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+
+private:
+    int _size;
+    LinkedNode* _dummyHead;
+
+};
