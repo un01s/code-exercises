d9b1164333c726bef420dd91c12d4f42322f2872
un01s <96022876+un01s@users.noreply.github.com>
2023-12-18 09:26:29 -0700

    update

---
+++ ex05/05-142.cpp
@@ -0,0 +1,35 @@
+/*
+ * leetcode 142
+ * 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            // fast and slow meet
+            if (slow == fast) {
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2; // entry to the ring
+            }
+        }
+        return nullptr;
+    }
+};
