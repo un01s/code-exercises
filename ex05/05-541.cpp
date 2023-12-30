c928b8a7769425d2b46c51e2791639667d9b29fe
un01s <96022876+un01s@users.noreply.github.com>
2023-12-30 09:37:02 -0700

    update

--- ex05/05-541.cpp
+++ ex05/05-541.cpp
@@ -20,3 +20,23 @@ public:
         return s;
     }
 };
+
+// create reverse instead of using lib
+class Solution {
+public:
+    void reverse(string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            if (i + k <= s.size()) {
+                reverse(s, i, i + k - 1);
+                continue;
+            }
+            reverse(s, i, s.size() - 1);
+        }
+        return s;
+    }
+};
