1fe82efda9bfe1f7f13bdc48fece8588e4dcebb9
un01s <96022876+un01s@users.noreply.github.com>
2023-12-29 18:46:02 -0700

    update

---
+++ ex04/04-1897.cpp
@@ -0,0 +1,21 @@
+/*
+ * leetcode 1897 
+ * redistribute characters to make all strings equal
+ *
+ */
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        int n=words.size();
+        if (n==1) return 1;
+        int freq[26]={0};
+        for(auto& s: words)
+            for(char c: s)
+                freq[c-'a']++;
+        for (int i=0; i<26; i++)
+            if (freq[i]%n!=0) return 0;
+        return 1;    
+    }
+};
+
