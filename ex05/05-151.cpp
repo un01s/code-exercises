d9cb2cec5e361680d9842d8e912a2b4f449aa279
un01s <96022876+un01s@users.noreply.github.com>
2024-01-01 14:11:39 -0700

    update

--- ex05/05-151.cpp
+++ ex05/05-151.cpp
@@ -52,6 +52,7 @@ public:
     }
 };
 
+// just one of the function 
 void removeExtraSpaces(string& s) {
     for (int i = s.size() - 1; i > 0; i--) {
         if (s[i] == s[i - 1] && s[i] == ' ') {
