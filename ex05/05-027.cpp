d00e52bf6b19a750d4e8494e04851a81045c1b4c
un01s <96022876+un01s@users.noreply.github.com>
2024-01-03 13:34:06 -0700

    update

---
+++ ex05/05-027.cpp
@@ -0,0 +1,19 @@
+/*
+ * leetcode 27
+ * double-pointer
+ *
+ */
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (val != nums[fastIndex]) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
+
