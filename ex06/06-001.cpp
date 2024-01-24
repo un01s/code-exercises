6a9375994f3c2817a13d29d36f015c60c6a95f9c
un01s <96022876+un01s@users.noreply.github.com>
2024-01-24 10:21:38 -0700

    update

---
+++ ex06/06-001.cpp
@@ -0,0 +1,40 @@
+/*
+ * 001 two sum
+ *
+ */
+
+// brute force
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (nums[i]+nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // no solution found
+    }
+};
+
+// unordered map
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        int tmp;
+
+        for (int i = 0; i < n; i++) {
+            tmp = target - nums[i];
+            if (mp.count(tmp)) {
+                return {i, mp[tmp]};
+            }            
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
