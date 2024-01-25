e2ba8fe4096d59ff328e63ad2901aef94ce3dfa3
un01s <96022876+un01s@users.noreply.github.com>
2024-01-24 18:47:15 -0700

    update

--- ex06/06-001.cpp
+++ ex06/06-001.cpp
@@ -20,7 +20,7 @@ public:
     }
 };
 
-// unordered map
+// unordered map: count(x)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -38,4 +38,21 @@ public:
         return {};
     }
 };
+
+// unordered_map find(x)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++) {
+            int c = target - nums[i];
+            if (m.find(c)!=m.end()) {
+                return {m[c], i};
+            }
+            m[nums[i]] = i;
+        }    
+        return {};
+    }
+};
+
 
