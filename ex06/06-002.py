b3cbf02e16ee7cb3fa5a6f0eae0983d800a0ce00
un01s <96022876+un01s@users.noreply.github.com>
2024-01-25 10:02:59 -0700

    update

---
+++ ex06/06-002.py
@@ -0,0 +1,19 @@
+"""
+leetcode 20
+valid parentheses
+
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        # key:value
+        mapping = {')':'(', '}':'{', ']':'['}
+        for c in s:
+            if c in mapping.values():
+                stack.append(c)
+            elif c in mapping.keys
+                if not stack or stack.pop() != mapping[c]:
+                    return false
+        return len(stack)==0
+
