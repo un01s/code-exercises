dcda68df0d819f2c44e4af98823db43b651a49fb
un01s <96022876+un01s@users.noreply.github.com>
2024-01-27 09:40:16 -0700

    update

--- ex06/06-004.cpp
+++ ex06/06-004.cpp
@@ -33,3 +33,19 @@ public:
     }
 };
 
+// DP
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(2, vector<int>(2)); 
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i % 2][0] = max(dp[(i - 1) % 2][0], -prices[i]);
+            dp[i % 2][1] = max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
+        }
+        return dp[(len - 1) % 2][1];
+    }
+};
+
