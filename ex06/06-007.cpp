de11f4289369c232c15a290e97cd45dc839ac7c1
un01s <96022876+un01s@users.noreply.github.com>
2024-02-03 12:47:14 -0700

    update

--- ex06/06-007.cpp
+++ ex06/06-007.cpp
@@ -31,3 +31,25 @@ public:
     }
 };
 
+// hash-table in an array
+// O(n) time, O(1) space complexity
+//
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int record[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            record[s[i]-'a']++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            record[t[i]-'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (record[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
