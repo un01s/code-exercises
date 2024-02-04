d5dfbf6003877dab120ac0dcfb7667fd894c23db
un01s <96022876+un01s@users.noreply.github.com>
2024-02-04 10:02:25 -0700

    update

---
+++ ex06/06-008.cpp
@@ -0,0 +1,27 @@
+/*
+ * leetcode 704
+ * binary search
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i=0;
+        int j=nums.size()-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+            
+        }
+        return -1;    
+    }
+};
+
