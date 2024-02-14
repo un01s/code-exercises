190cf78bd276323fdc4f5fa7870e5d9052369ce9
un01s <96022876+un01s@users.noreply.github.com>
2024-02-14 06:27:33 -0700

    update

---
+++ ex06/06-018.cpp
@@ -0,0 +1,34 @@
+/*
+ * leetcode 206 
+ * reverse linked list
+ *
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=nullptr ;
+        ListNode* current = head;
+        ListNode* next=nullptr ;
+
+        while (current != nullptr) {
+            next = current->next;  // Store the next node
+            current->next = prev;      // Reverse the pointer
+            prev = current;            // Move prev to current node
+            current = next;        // Move current to next node
+        }
+
+        return prev;    
+    }
+};
+
