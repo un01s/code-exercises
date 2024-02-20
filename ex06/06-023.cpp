4e4117b5a58fbfc5066ab5cca35929e3cae55fbf
un01s <96022876+un01s@users.noreply.github.com>
2024-02-20 09:40:18 -0700

    update

---
+++ ex06/06-023.cpp
@@ -0,0 +1,26 @@
+/*
+ * leetcode 104
+ * max depth of binary tree
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        // recursion
+        if (root == nullptr) return 0;
+        else return 1+max(maxDepth(root->left), maxDepth(root->right));    
+    }
+};
+
