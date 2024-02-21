1e88a5c4082439bb8e78c73c7273761b032bc575
un01s <96022876+un01s@users.noreply.github.com>
2024-02-21 09:38:34 -0700

    update

---
+++ ex06/06-024.cpp
@@ -0,0 +1,32 @@
+/*
+ * leetcode 217
+ * contains duplicate
+ *
+ */
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }    
+        for (auto &p: mp) {
+            if (p.second > 1) return true;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int num: nums) {
+            if (mp.count(num) > 0) return true;
+            mp.insert(num);
+        }
+        return false;
+    }
+};
+
