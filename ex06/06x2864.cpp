dde45460fea00eefeee9339f31e131d150e3ac05
un01s <96022876+un01s@users.noreply.github.com>
2024-02-29 18:25:20 -0700

    update

---
+++ ex06/06x2864.cpp
@@ -0,0 +1,13 @@
+/*
+ * leetcode 2864
+ * maximum odd binary number
+ */
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n=s.size(), n1=count(s.begin(), s.end(), '1'), n0=n-n1;
+        return string(n1-1, '1')+string(n0, '0')+string(1, '1');    
+    }
+};
+
