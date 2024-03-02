7b39a81147715dfd22e0cf317cbccc7bd5c4ffae
un01s <96022876+un01s@users.noreply.github.com>
2024-03-02 09:31:10 -0700

    update

---
+++ ex07/07-double-pointer-005.cpp
@@ -0,0 +1,35 @@
+/*
+ * leetcode 80
+ * remove duplicates from sorted array II
+ *
+ * double-pointer, in-place
+ * fast-slow or left-right pointer
+ * 
+ */
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if (nums.size()==0) return 0;
+        if (nums.size()==1) return 1;
+        
+        int i=1;
+        for (int j=0; j<nums.size(); j++)
+        {
+            if (j==0 || j==1) 
+                continue;
+            else if (nums[j]==nums[i] && nums[j]==nums[i-1])
+                continue;
+            else
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        
+        return i+1;
+        
+    }
+};
+
