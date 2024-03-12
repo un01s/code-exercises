3e9e00a674dda4664fecd0483be08dbebf68cee8
un01s <96022876+un01s@users.noreply.github.com>
2024-03-12 09:56:56 -0600

    update

--- ex07/07-double-pointer-016.cpp
+++ ex07/07-double-pointer-016.cpp
@@ -30,3 +30,27 @@ public:
     }
 };
 
+// double-pointer
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        const int mod = 1e9 + 7;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        vector<int> pow2mod(n + 1, 1);
+        for (int exp = 1; exp <= n; exp++)
+            pow2mod[exp] = (pow2mod[exp - 1] << 1) % mod;
+
+        int l = 0, r = n - 1;
+        int result = 0;
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                result = (result + pow2mod[r - l]) % mod;
+                l++;
+            } else
+                r--;
+        }
+        return result;
+    }
+};
