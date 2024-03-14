52bd6892f01b2649a36a0e1df061afcb5adbdf9a
un01s <96022876+un01s@users.noreply.github.com>
2024-03-13 18:12:46 -0600

    update

---
+++ ex06/06x930.cpp
@@ -0,0 +1,32 @@
+/*
+ * leetcode 930 
+ * binary subarrays with sum
+ *
+ */
+
+class Solution {
+public:
+    int atMost(vector<int>& v, int target, int n) {
+        if (target < 0)
+            return 0;
+        int i = 0, j = 0, sm = 0, ans = 0;
+
+        while (j < n) {
+            sm += v[j];
+            while (sm > target) {
+                sm -= v[i];
+                ++i;
+            }
+            int cur_len = j - i + 1;
+            ans += (cur_len);
+            ++j;
+        }
+
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        return atMost(nums, goal, n) - atMost(nums, goal - 1, n);
+    }
+};
+
