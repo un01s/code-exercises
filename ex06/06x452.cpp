d15f5ea5421ba736609beb45a53d32eb7afa7e76
un01s <96022876+un01s@users.noreply.github.com>
2024-03-17 18:21:52 -0600

    update

---
+++ ex06/06x452.cpp
@@ -0,0 +1,28 @@
+/*
+ * leetcode 452
+ * minimum number of arrows to burst balloons
+ *
+ */
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(),
+             [](const auto& a, const auto& b) { return a[0] < b[0]; });
+
+        int arrows = 1;
+        int end = points[0][1];
+
+        for (size_t i = 1; i < points.size(); ++i) {
+            if (points[i][0] > end) {
+                arrows++;
+                end = points[i][1];
+            } else {
+                end = std::min(end, points[i][1]);
+            }
+        }
+
+        return arrows;
+    }
+};
+
