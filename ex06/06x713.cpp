5fd4ad4ed717cd4e95aa22c3e4789ff44713a889
un01s <96022876+un01s@users.noreply.github.com>
2024-03-27 07:52:12 -0600

    update

---
+++ ex06/06x713.cpp
@@ -0,0 +1,23 @@
+/*
+ * leetcode 713
+ * subarray product less than K
+ *
+ */
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1)
+            return 0; // edge case
+        unsigned long long prod = 1;
+        int n = nums.size(), l = 0, cnt = 0;
+        for (int r = 0; r < n; r++) {
+            prod *= nums[r];
+            while (prod >= k)
+                prod /= nums[l++];
+            cnt += (r - l + 1); // # of valid subarrays for [l, r] by varying l
+        }
+        return cnt;
+    }
+};
+
