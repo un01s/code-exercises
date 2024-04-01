7fc66ee7eea23cb088720aadec9aedecd7cb0a8e
un01s <96022876+un01s@users.noreply.github.com>
2024-04-01 09:21:18 -0600

    update

---
+++ ex06/06x704.cpp
@@ -0,0 +1,30 @@
+/*
+ * leetcode 704 binary search in an array (vector)
+ *
+ * array, binary search
+ * keep the range an invariant
+ * either [left, right] or [left, right)
+ * 
+ * the solution below is using [left, right)
+ *
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+        while (l < r) {
+            int m = l + (r-l)/2;
+            if (nums[m] > target) {
+                r = m;
+            } else if (nums[m] < target) {
+                l = m+1;
+            } else {
+                return m;
+            }
+        }    
+        return -1;
+    }
+};
+
