8ac5e66e24305df84a425517ffe1baa42139a316
un01s <96022876+un01s@users.noreply.github.com>
2024-04-02 09:59:18 -0600

    update

---
+++ ex06/06x203.cpp
@@ -0,0 +1,38 @@
+/*
+ * leetcode 203
+ * remove linked list elements
+ *
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0); // set up a dummy head
+        dummyHead->next = head;                // head pointed to dummy
+
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+};
+
