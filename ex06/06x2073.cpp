26ec90f3d7ce2ad27a90121e899e9861cbf8f357
un01s <96022876+un01s@users.noreply.github.com>
2024-04-09 09:31:54 -0600

    update

---
+++ ex06/06x2073.cpp
@@ -0,0 +1,19 @@
+/*
+ * leetcode 2073
+ * 
+ */
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int x = tickets[k];
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            int buy = (i > k) ? x - 1 : x;
+            time += min(buy, tickets[i]);
+        }
+        return time;
+    }
+};
+
