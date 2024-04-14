e0302fcc1153fe227dbbb95f71b16037356bc6dd
un01s <96022876+un01s@users.noreply.github.com>
2024-04-14 13:41:40 -0600

    update

--- ex07/07-double-pointer-028.cpp
+++ ex07/07-double-pointer-028.cpp
@@ -26,3 +26,32 @@ public:
     }
 };
 
+// DP
+class Solution2 {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        int i = 0;
+        int ret = 0;
+        for (int j=0; j<n; j++)
+        {
+            if (nums[j]==1)
+            {
+                ret = max(ret, j-i+1);
+                continue;
+            }
+                
+            count++;
+            while (count>k)
+            {
+                if (nums[i]==0)
+                    count--;
+                i++;
+            }
+            ret = max(ret, j-i+1);
+        }
+        return ret;
+    }
+};
+
