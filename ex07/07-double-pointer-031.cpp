0ecaac9069746472abf72a645c549635b21039dd
un01s <96022876+un01s@users.noreply.github.com>
2024-04-22 10:53:14 -0600

    update

---
+++ ex07/07-double-pointer-031.cpp
@@ -0,0 +1,25 @@
+/*
+ * leetcode 1838
+ * frequency of the most frequent element
+ * sliding window
+ */
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        long count = 0;
+        int j = 0;
+        int ret = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            count += (long)(i - j) * (long)(nums[i] - nums[i - 1]);
+            while (count > k) {
+                count -= nums[i] - nums[j];
+                j++;
+            }
+            ret = max(ret, i - j + 1);
+        }
+        return ret;
+    }
+};
