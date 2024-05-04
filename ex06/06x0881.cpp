6cd89ad9bd3d9eb4adfbd6c0b5d126763537a620
un01s <96022876+un01s@users.noreply.github.com>
2024-05-03 20:07:23 -0600

    update

---
+++ ex06/06x0881.cpp
@@ -0,0 +1,31 @@
+/*
+ * leetcode 881
+ * boats to save people
+ *
+ */
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        unsigned freq[30001] = {0};
+        int maxW = 0, minW = 30001;
+        for (int x : people) {
+            freq[x]++;
+            maxW = max(maxW, x);
+            minW = min(minW, x);
+        }
+        for (int i = minW, j = 0; i <= maxW; i++) {
+            int f = freq[i];
+            fill(people.begin() + j, people.begin() + j + f, i);
+            j += f;
+        }
+        int x = 0;
+        for (int l = 0, r = people.size() - 1; l <= r; r--) {
+            x++;
+            if (people[l] + people[r] <= limit)
+                l++;
+        }
+        return x;
+    }
+};
+
