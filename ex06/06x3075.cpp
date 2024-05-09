e6e73922cca4bc2a968e1977e675155cfd785edd
un01s <96022876+un01s@users.noreply.github.com>
2024-05-08 18:38:23 -0600

    update

---
+++ ex06/06x3075.cpp
@@ -0,0 +1,22 @@
+/*
+ * leetcode 3075
+ * maximize happiness of selected children
+ *
+ */
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        int n = happiness.size();
+        priority_queue<int> pq(happiness.begin(), happiness.end()); // maxheap
+        long long sum = 0;
+        for (int i = 0; i < k; i++) {
+            long long x = max(0, pq.top() - i);
+            pq.pop();
+            sum += x;
+        }
+
+        return sum;
+    }
+};
+
