35d1f7a14e95a55c7f2c0e489c340cce9f30cce1
un01s <96022876+un01s@users.noreply.github.com>
2024-05-11 18:15:58 -0600

    update

---
+++ ex06/06x2373.cpp
@@ -0,0 +1,30 @@
+/*
+ * leetcode 2373 
+ * largest local values in a matrix
+ *
+ */
+
+class Solution {
+public:
+    int helper(int i, int j, vector<vector<int>>& grid) {
+        int ans = 0;
+
+        for (int ii = 0; ii < 3; ii++)
+            for (int jj = 0; jj < 3; jj++)
+                ans = max(ans, grid[i + ii][j + jj]);
+
+        return ans;
+    }
+
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int ln = grid.size();
+        vector<vector<int>> ans(ln - 2, vector<int>(ln - 2));
+
+        for (int i = 0; (i + 3) <= ln; i++)
+            for (int j = 0; (j + 3) <= ln; j++)
+                ans[i][j] = helper(i, j, grid);
+
+        return ans;
+    }
+};
+
