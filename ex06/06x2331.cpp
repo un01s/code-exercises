09839b31e7ef9e1b240f907e9c2625636d062c5d
un01s <96022876+un01s@users.noreply.github.com>
2024-05-15 20:10:50 -0600

    update

---
+++ ex06/06x2331.cpp
@@ -0,0 +1,31 @@
+/*
+ * leetcode 2331
+ * evaluate boolean binary tree
+ *
+ * DFS recursive
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if (!root->left && !root->right)
+            return root->val;
+        bool l = evaluateTree(root->left);
+        bool r = evaluateTree(root->right);
+        return (root->val == 3) ? l & r : l | r;
+    }
+};
+
