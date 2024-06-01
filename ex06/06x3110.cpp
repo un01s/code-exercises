2894f3b113e2520ded4048fab47996bdb7834218
un01s <96022876+un01s@users.noreply.github.com>
2024-05-31 19:55:25 -0600

    update

---
+++ ex06/06x3110.cpp
@@ -0,0 +1,16 @@
+/*
+ * leetcode 3110
+ *
+ */
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++)
+            ans += abs(s[i] - s[i - 1]);
+        return ans;
+    }
+};
+
